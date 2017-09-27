3d4d43940f8ca4d5b7dbc450480a31dfdd556797
florent champigny <florent.champigny@backelite.com>
2017-09-27 21:27:37 +0200

    update sample

--- app/src/main/java/com/github/florent37/materialimageanimation/sample/MaterialImageLoadingMainActivity.java
+++ app/src/main/java/com/github/florent37/materialimageanimation/sample/MaterialImageLoadingMainActivity.java
@@ -1,9 +1,7 @@
 package com.github.florent37.materialimageanimation.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.github.florent37.materialimageanimation.R;
@@ -17,7 +15,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MaterialImageLoadingMainActivity extends AppCompatActivity {
 
     final static int DURATION = 4000;
 
@@ -29,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.materialimageloading_activity_main);
         ButterKnife.bind(this);
     }
 
@@ -40,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadImage(){
-        Picasso.with(this).load(R.drawable.image_sample).fit().centerCrop().into(imageView, new Callback() {
+        Picasso.with(this).load(R.drawable.materialimageloading_image_sample).fit().centerCrop().into(imageView, new Callback() {
 
             @Override
             public void onSuccess() {
